84b7f732238bb7412b8e62766369b4fd97fa64c2
Razeihk <rasec.tracip@gmail.com>
2015-05-28 10:51:56 +0200

    Ma scène

---
+++ Assets/Scripts/Saut.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saut : MonoBehaviour {
+
+    Rigidbody myRigidbody;
+    public float force;
+    public float speed;
+
+    float HORIZONTAL;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        HORIZONTAL = Input.GetAxis("Horizontal");
+
+        myRigidbody.AddForce(3 * Physics.gravity);
+        if(Input.GetMouseButtonDown(0))
+        {
+            myRigidbody.velocity = new Vector3(0, force, 0);
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.position += transform.right / speed;
+        }
+	}
+}
