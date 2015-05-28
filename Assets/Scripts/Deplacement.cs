279fd46b063d0a9aaf2816b397f3b9cae0eedc0c
Mikozouzou <antonin.adeline@gmail.com>
2015-05-28 10:43:03 +0200

    Ajout du script de deplacement
    Tout est dans le titre

---
+++ Assets/Scripts/Deplacement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Deplacement : MonoBehaviour {
+
+    Rigidbody myRigidbody;
+    public float jumpValue;
+
+	// Use this for initialization
+	void Start () 
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            myRigidbody.AddForce(transform.position.x, jumpValue, transform.position.z); 
+        }
+	}
+}
