74515746001564450bdf2ed5412e6b777d9d1ad3
Mikozouzou <antonin.adeline@gmail.com>
2015-05-29 15:53:55 +0200

    Intégration du tir
    Problème lié au focus de l'arme quand visée dans le vide

---
+++ Assets/Scripts/Projectile_Script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_Script : MonoBehaviour {
+
+    public float speed;
+    public float lifetime;
+    public Transform canonPosition;
+    Rigidbody myRigidbody; 
+
+	// Use this for initialization
+	void Start () 
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        Destroy(this, lifetime);
+        //transform.position = canonPosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        myRigidbody.AddForce(canonPosition.forward * speed);
+	}
+}
